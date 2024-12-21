4920e19f106b340a4dbbea9b9c63e3e80533c636
kiran8884146600 <143705462+kiran8884146600@users.noreply.github.com>
2024-12-21 13:56:22 +0530

    Create remot-exce.tf

---
+++ remot-exce.tf
@@ -0,0 +1,23 @@
+resource "aws_instance" "instance_1" {
+    ami = "ami-0e2c8caa4b6378d8c"  
+    instance_type = "t2.micro" 
+    key_name= "aws_key"
+    subnet_id       = aws_subnet.public_2.id
+    security_groups = [aws_security_group.instances.id]
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt install -y nginx",
+      "sudo systemctl start nginx",
+      "sudo systemctl enable nginx"
+    ]
+  }
+  connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ubuntu"
+      private_key = file("/home/user/Downloads/keys/aws/aws_key")
+      timeout     = "4m"
+   }
+}
