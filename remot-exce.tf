7064a12a3f47b3b3fbfffc5e95e05e6433fda919
kiran8884146600 <143705462+kiran8884146600@users.noreply.github.com>
2024-12-21 15:44:01 +0530

    Update remot-exce.tf

--- remot-exce.tf
+++ remot-exce.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "instance_1" {
     ami = "ami-0e2c8caa4b6378d8c"  
     instance_type = "t2.micro" 
     key_name= "aws_key"
-    subnet_id       = aws_subnet.public_2.id
+    subnet_id       = aws_subnet.public_1a.id,aws_subnet.public_1b.id
     security_groups = [aws_security_group.instances.id]
 
   provisioner "remote-exec" {
